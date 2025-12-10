16154ca04af8ce322d69a540bcb76e507144adcd
Samudra Zulqifli <samudrazulqifli24@gmail.com>
2025-12-10 10:35:41 +0700

    update discord log latency

--- src/lib/discord-logger.ts
+++ src/lib/discord-logger.ts
@@ -10,6 +10,7 @@ export async function sendErrorToDiscord(
     response,
     request,
     latency,
+    lateResponse,
   }: {
     site?: string;
     endpoint?: string;
@@ -20,14 +21,17 @@ export async function sendErrorToDiscord(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     request?: any;
     latency?: number;
+    lateResponse?: boolean;
   } = {}
 ) {
   try {
     const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || "https://discord.com/api/webhooks/1443424714880974878/VylYCP2MWALWvZnI0NwTBoOyKhSrFRpiYcKCzaNLljAHOzjojsY5bH10AgL189P09hzc";
+    const WEBHOOK_LATE_RESPONSE = "https://discord.com/api/webhooks/1447756377400414268/b7TWJxS_iM9bjONBOP5ZbrYiuJ128StUBW9XFpDQhn2dkrEDAYrdRri1yB2r5MSR2iSR";
     const NODE_ENV = process.env.NODE_ENV;
 
     if (!WEBHOOK_URL) return;
-    if (NODE_ENV === "development") return;
+    if (!WEBHOOK_LATE_RESPONSE) return;
+    // if (NODE_ENV === "development") return;
 
     // Small rate limiting (1 log / 1.5 detik)
     if (Date.now() - lastSent < 1500) return;
@@ -40,7 +44,7 @@ export async function sendErrorToDiscord(
       return json.length > 1500 ? json.slice(0, 1500) + "...\n(TRUNCATED)" : json;
     };
 
-    await fetch(WEBHOOK_URL, {
+    await fetch(lateResponse ? WEBHOOK_LATE_RESPONSE : WEBHOOK_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
