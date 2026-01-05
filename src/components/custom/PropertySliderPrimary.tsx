bf2c804bea8f2c3f50afa5aba37201b8ed430a67
Samudra Zulqifli <samudrazulqifli24@gmail.com>
2026-01-05 14:40:34 +0700

    try to delete

--- src/components/custom/PropertySliderPrimary.tsx
+++ src/components/custom/PropertySliderPrimary.tsx
@@ -19,7 +19,7 @@ export default function PropertySliderPrimary(props: PropertyPrimarySliderProps)
     <div className="w-full flex flex-col lg:flex-row lg:items-stretch items-center gap-10 md:pt-28 lg:pt-12 pt-0">
       <div className="lg:w-1/4 w-full h-full flex flex-col gap-5 text-center lg:text-left justify-between">
         <div className="text-3xl font-extrabold w-full">{props.Title}</div>
-        <div className="font-normal">{props.Description}</div>
+        {/* <div className="">{props.Description}</div> */}
         <div className="sm:block hidden">
           <Link href={"/perumahan-baru"} className="bg-secondary rounded-full font-bold text-label-4xl text-white px-8 py-2">
             Lihat Semua
