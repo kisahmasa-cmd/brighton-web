b148090abbba642ac9f131625154e2c5580db1be
Samudra Zulqifli <samudrazulqifli24@gmail.com>
2026-01-05 15:00:36 +0700

    try to add fallback

--- src/components/custom/PropertySliderPrimary.tsx
+++ src/components/custom/PropertySliderPrimary.tsx
@@ -16,7 +16,7 @@ export default function PropertySliderPrimary(props: PropertyPrimarySliderProps)
     <div className="w-full flex flex-col lg:flex-row lg:items-stretch items-center gap-10 md:pt-28 lg:pt-12 pt-0">
       <div className="lg:w-1/4 w-full h-full flex flex-col gap-5 text-center lg:text-left justify-between">
         <h2 className="text-3xl font-extrabold w-full">{props.Title}</h2>
-        <p className="font-normal">{props.Description}</p>
+        <p className="font-normal">{props.Description ?? "Tidak ada deskripsi"}</p>
         <div className="sm:block hidden">
           <Link href={"/perumahan-baru"} className="bg-secondary rounded-full font-bold text-label-4xl text-white px-8 py-2">
             Lihat Semua
